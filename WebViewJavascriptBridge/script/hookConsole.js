5e815f066d1f969417dbcda69361cde531a7ce0a
syxc <nian1@duck.com>
2024-07-10 20:23:52 +0800

    Update hookConsole.js

--- WebViewJavascriptBridge/script/hookConsole.js
+++ WebViewJavascriptBridge/script/hookConsole.js
@@ -1,50 +1,50 @@
 ; (function (window) {
-  if (window.hookConsole) return;
-  const consoleHandler = window.webkit.messageHandlers.console;
-  if (!consoleHandler || typeof consoleHandler !== 'object') {
-    console.warn('consoleHandler is not defined or not an object');
+  if (window.hookConsole) {
+    console.log("hook Console have already finished.");
     return;
   }
 
-  function handleCircularReference(key, value) {
-    if (typeof value === 'object' && value !== null) {
-      return '<<Circular Reference>>';
-    }
-    return value;
+  if (!window.webkit.messageHandlers || !window.webkit.messageHandlers.console) {
+    console.warn("consoleHandler not found, unable to send message to native.");
+    return;
   }
 
-  const formatMessage = (function () {
-    const cache = {};
-    Object.freeze(cache);
-    return function (obj) {
-      const key = JSON.stringify(obj, handleCircularReference);
-      return cache[key] || (cache[key] = {
-        type: typeof obj,
-        value: obj instanceof Date ? obj.getTime() : Array.isArray(obj) ? obj.toString() : JSON.stringify(obj, handleCircularReference)
-      });
-    };
-  })();
-
-  const originalConsoleLogFunc = window.console.log;
-  window.hookConsole = true;
-  window.console.log = function () {
-    const args = Array.prototype.slice.call(arguments);
-    originalConsoleLogFunc.apply(window.console, args);
-
-    const messageStr = JSON.stringify(args.map(formatMessage).map(function (message) { return message.value; }));
-    if (messageStr) {
-      const send = function () {
-        try {
-          consoleHandler.postMessage(encodeURIComponent(messageStr));
-        } catch (e) {
-          console.error('Error sending console log to native: ' + e.stack);
-        }
-      };
-
-      // 使用 Promise 或同步方式发送
-      'Promise' in window ? new Promise(send).catch(function (e) { console.error(e); }) : send();
+  function printObject(obj) {
+    if (typeof obj === 'object') {
+      return JSON.stringify(obj);
+    } else {
+      return "" + obj;
     }
-  };
+  }
 
+  function getObjectString(obj) {
+    let message;
+    if (obj === null) {
+      message = "null";
+    } else if (typeof obj === "undefined") {
+      message = "undefined";
+    } else if (obj instanceof Promise) {
+      message = "This is a javascript Promise.";
+    } else if (obj instanceof Date) {
+      message = obj.getTime().toString();
+    } else if (obj instanceof Array) {
+      message = '[' + obj.toString() + ']';
+    } else {
+      message = printObject(obj);
+    }
+    return message;
+  }
+
+  console.log("start hook Console.");
+  window.console.log = (function (oriLogFunc) {
+    window.hookConsole = true;
+    return function () {
+      for (let obj of Array.from(arguments)) {
+        oriLogFunc.apply(window.console, [obj]);
+        const message = getObjectString(obj);
+        window.webkit.messageHandlers.console.postMessage(message);
+      }
+    };
+  })(window.console.log, printObject);
   console.log("end hook Console.");
 })(window);

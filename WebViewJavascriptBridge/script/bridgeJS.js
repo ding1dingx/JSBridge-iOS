b48d1a947d3d68e4c66f0e2f49187c840a02659a
syxc <nian1@duck.com>
2024-07-10 17:11:04 +0800

    Update bridgeJS.js
    调整过后的版本，JavaScript ES5 语法，提升兼容性。

--- WebViewJavascriptBridge/script/bridgeJS.js
+++ WebViewJavascriptBridge/script/bridgeJS.js
@@ -1,55 +1,58 @@
 ; (function (window) {
-  if (window.WebViewJavascriptBridge) {
-    return;
-  }
+  if (window.WebViewJavascriptBridge) { return; }
+
   window.WebViewJavascriptBridge = {
     registerHandler: registerHandler,
     callHandler: callHandler,
     handleMessageFromNative: handleMessageFromNative
   };
-  let messageHandlers = {};
-  let responseCallbacks = {};
-  let uniqueId = 1;
+
+  var messageHandlers = {};
+  var responseCallbacks = {};
+  var uniqueId = 1;
+
   function registerHandler(handlerName, handler) {
     messageHandlers[handlerName] = handler;
   }
+
   function callHandler(handlerName, data, responseCallback) {
-    if (arguments.length === 2 && typeof data == 'function') {
+    if (arguments.length === 2 && typeof data === 'function') {
       responseCallback = data;
       data = null;
     }
     doSend({ handlerName: handlerName, data: data }, responseCallback);
   }
+
   function doSend(message, responseCallback) {
     if (responseCallback) {
-      const callbackId = 'cb_' + (uniqueId++) + '_' + new Date().getTime();
+      var callbackId = 'cb_' + (uniqueId++) + '_' + new Date().getTime();
       responseCallbacks[callbackId] = responseCallback;
-      message['callbackId'] = callbackId;
+      message.callbackId = callbackId;
     }
     window.webkit.messageHandlers.normal.postMessage(JSON.stringify(message));
   }
+
   function handleMessageFromNative(messageJSON) {
-    const message = JSON.parse(messageJSON);
-    let responseCallback;
+    var message = JSON.parse(messageJSON);
+    var responseCallback;
     if (message.responseId) {
       responseCallback = responseCallbacks[message.responseId];
-      if (!responseCallback) {
-        return;
+      if (responseCallback) {
+        responseCallback(message.responseData);
+        delete responseCallbacks[message.responseId];
       }
-      responseCallback(message.responseData);
-      delete responseCallbacks[message.responseId];
     } else {
       if (message.callbackId) {
-        const callbackResponseId = message.callbackId;
+        var callbackResponseId = message.callbackId;
         responseCallback = function (responseData) {
           doSend({ handlerName: message.handlerName, responseId: callbackResponseId, responseData: responseData });
         };
       }
-      const handler = messageHandlers[message.handlerName];
-      if (!handler) {
-        console.log("WebViewJavascriptBridge: WARNING: no handler for message from ObjC:", message);
-      } else {
+      var handler = messageHandlers[message.handlerName];
+      if (handler) {
         handler(message.data, responseCallback);
+      } else {
+        console.warn("[WebViewJavascriptBridge] => WARNING: no handler for message from Swift/ObjC:", message);
       }
     }
   }

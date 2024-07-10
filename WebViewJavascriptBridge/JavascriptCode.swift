c1f340d440e883a52d96431fb88fadd09323badc
syxc <nian1@duck.com>
2024-07-11 00:25:28 +0800

    Update JavascriptCode.swift

--- WebViewJavascriptBridge/JavascriptCode.swift
+++ WebViewJavascriptBridge/JavascriptCode.swift
@@ -12,7 +12,9 @@ enum JavascriptCode {
         let bridgeScript =
             """
             ; (function (window) {
-              if (window.WebViewJavascriptBridge) { return; }
+              if (window.WebViewJavascriptBridge) {
+                return;
+              }
 
               const messageHandlers = {};
               const responseCallbacks = {};
@@ -25,68 +27,70 @@ enum JavascriptCode {
 
                 callHandler(handlerName, data, responseCallback) {
                   if (arguments.length === 2 && typeof data === 'function') {
-                    // 两个参数，第二个参数是函数
                     responseCallback = data;
                     data = null;
-                  } else if (arguments.length === 3 && typeof data !== 'function' && typeof responseCallback === 'function') {
-                    // 三个参数，第二个参数不是函数，第三个参数是函数，此处无需添加额外的处理代码，因为已经符合预期
-                  } else {
-                    // 如果参数数量或类型不匹配，则抛出错误
-                    console.error('Invalid arguments for callHandler:', arguments);
-                    return;
                   }
-                  const callbackResult = responseCallback ? responseCallback() : null;
-                  doSend({ handlerName, data }, callbackResult);
+                  this.doSend({ handlerName, data }, responseCallback);
                 },
 
                 handleMessageFromNative(messageJSON) {
-                  const message = JSON.parse(messageJSON);
-
-                  if (message.responseId) {
-                    if (responseCallbacks[message.responseId]) {
-                      // 如果 responseCallbacks 中存在 responseId 对应的回调函数，则调用它
-                      const responseCallback = responseCallbacks[message.responseId];
-                      try {
-                        responseCallback(message.responseData);
-                      } catch (error) {
-                        console.warn("[WebViewJavascriptBridge] => WARNING: error occurred in responseCallback:", error);
-                      } finally {
-                        // 无论是否发生错误，都要删除 responseCallbacks 中对应回调函数
-                        delete responseCallbacks[message.responseId];
-                      }
+                  try {
+                    const message = JSON.parse(messageJSON);
+                    if (message.responseId) {
+                      this.handleResponseMessage(message);
+                    } else if (message.callbackId) {
+                      this.handleCallbackMessage(message);
                     } else {
-                      // 如果 responseCallbacks 中不存在 responseId 对应的回调函数，则输出警告信息，并执行默认的处理逻辑
-                      console.warn("[WebViewJavascriptBridge] => WARNING: no callback found for responseId:", message.responseId);
-                      // 执行默认的处理逻辑，例如将消息丢弃...
-                    }
-                  } else {
-                    if (message.callbackId) {
-                      const callbackResponseId = message.callbackId;
-                      const handler = messageHandlers[message.handlerName];
-
-                      if (handler) {
-                        handler(message.data, responseData => {
-                          doSend({ handlerName: message.handlerName, responseId: callbackResponseId, responseData });
-                        });
-                      } else {
-                        console.warn("[WebViewJavascriptBridge] => WARNING: no handler for message from Swift/ObjC:", message);
-                      }
-                    } else {
-                      // 如果 message.callbackId 不存在，则不做任何操作
                       console.warn("[WebViewJavascriptBridge] => WARNING: message from Native does not contain callbackId:", message);
                     }
+                  } catch (error) {
+                    console.error("[WebViewJavascriptBridge] => ERROR: Failed to parse message from native:", error, messageJSON);
+                  }
+                },
+
+                doSend(sendMessage, responseCallback) {
+                  try {
+                    sendMessage.callbackId = responseCallback ? 'cb_' + (uniqueId++) + '_' + new Date().getTime() : null;
+                    responseCallbacks[sendMessage.callbackId] = responseCallback || null;
+                    if (window.webkit.messageHandlers.normal) {
+                      window.webkit.messageHandlers.normal.postMessage(JSON.stringify(sendMessage));
+                    } else {
+                      console.error("[WebViewJavascriptBridge] => ERROR: Unable to find the 'normal' message handler in WKWebView.");
+                    }
+                  } catch (error) {
+                    console.error("[WebViewJavascriptBridge] => ERROR: Failed to send message to native:", error, sendMessage);
+                  }
+                },
+
+                handleResponseMessage(message) {
+                  const responseCallback = responseCallbacks[message.responseId];
+                  if (responseCallback) {
+                    try {
+                      responseCallback(message.responseData);
+                    } catch (error) {
+                      console.error("[WebViewJavascriptBridge] => ERROR: Failed to execute response callback:", error, message);
+                    } finally {
+                      delete responseCallbacks[message.responseId];
+                    }
+                  }
+                },
+
+                handleCallbackMessage(message) {
+                  const callbackResponseId = message.callbackId;
+                  const handler = messageHandlers[message.handlerName];
+                  if (handler) {
+                    try {
+                      handler(message.data, responseData => {
+                        this.doSend({ handlerName: message.handlerName, responseId: callbackResponseId, responseData });
+                      });
+                    } catch (error) {
+                      console.error("[WebViewJavascriptBridge] => ERROR: Failed to execute callback handler:", error, message);
+                    }
+                  } else {
+                    console.warn("[WebViewJavascriptBridge] => WARNING: no handler for message from Swift/ObjC:", message);
                   }
                 }
               };
-
-              function doSend(message, responseCallback) {
-                // 生成一个新的 callbackId
-                message.callbackId = 'cb_' + (uniqueId++) + '_' + new Date().getTime();
-                // 如果 responseCallback 为 null 或 undefined，则将其设置为 null
-                responseCallbacks[message.callbackId] = responseCallback || null;
-                // 将消息发送到 Native 端
-                window.webkit.messageHandlers.normal.postMessage(JSON.stringify(message));
-              }
 
             })(window);
             """

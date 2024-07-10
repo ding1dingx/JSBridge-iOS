b053511a046d661ac546be070186eb7e9b394d2b
syxc <nian1@duck.com>
2024-07-10 20:41:56 +0800

    Update hookConsole.js

--- WebViewJavascriptBridge/script/hookConsole.js
+++ WebViewJavascriptBridge/script/hookConsole.js
@@ -4,47 +4,34 @@
     return;
   }
 
-  if (!window.webkit.messageHandlers || !window.webkit.messageHandlers.console) {
+  const messageHandler = window.webkit.messageHandlers?.console;
+  if (!messageHandler) {
     console.warn("consoleHandler not found, unable to send message to native.");
     return;
   }
 
   function printObject(obj) {
-    if (typeof obj === 'object') {
-      return JSON.stringify(obj);
-    } else {
-      return "" + obj;
-    }
-  }
-
-  function getObjectString(obj) {
-    let message;
-    if (obj === null) {
-      message = "null";
-    } else if (typeof obj === "undefined") {
-      message = "undefined";
-    } else if (obj instanceof Promise) {
-      message = "This is a javascript Promise.";
+    if (obj instanceof Promise) {
+      return "This is a javascript Promise.";
     } else if (obj instanceof Date) {
-      message = obj.getTime().toString();
-    } else if (obj instanceof Array) {
-      message = '[' + obj.toString() + ']';
+      return obj.toLocaleString();
+    } else if (Array.isArray(obj)) {
+      return '[' + obj.map(item => printObject(item)).join(', ') + ']';
     } else {
-      message = printObject(obj);
+      return JSON.stringify(obj) || '';
     }
-    return message;
   }
 
   console.log("start hook Console.");
+  window.hookConsole = true;
   window.console.log = (function (oriLogFunc) {
-    window.hookConsole = true;
-    return function () {
-      for (let obj of Array.from(arguments)) {
-        oriLogFunc.apply(window.console, [obj]);
-        const message = getObjectString(obj);
-        window.webkit.messageHandlers.console.postMessage(message);
-      }
+    return function (...args) {
+      oriLogFunc.apply(window.console, args);
+      args.forEach(obj => {
+        const message = printObject(obj);
+        messageHandler.postMessage(message);
+      });
     };
-  })(window.console.log, printObject);
+  })(window.console.log);
   console.log("end hook Console.");
 })(window);
